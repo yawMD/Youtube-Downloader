5f30ae3652ab062f5d1a82a1447248eada14a877
Cato Perez <perezcatoc@gmail.com>
2022-06-20 18:02:06 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const ytdl = require("ytdl-core");
+app.set("view engine", "ejs");
+// OUR ROUTES WILL GO HERE
+
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+	return res.render("index");
+});
+
+app.get("/download", async (req, res) => {
+	const v_id = req.query.url.split('v=')[1];
+    const info = await ytdl.getInfo(req.query.url);
+	return res.render("download", {
+		url: "https://www.youtube.com/embed/" + v_id,
+        info: info.formats.sort((a, b) => {
+            return a.mimeType < b.mimeType;
+        }),
+	});
+});
+
+
+
+
+app.listen(3000, () => {
+	console.log("Server is running on http://localhost:3000");
+});
